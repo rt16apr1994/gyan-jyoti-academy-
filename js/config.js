fc50211382c02385eeec1a4c56ef11102e4881e5
rt16apr1994 <108141781+rt16apr1994@users.noreply.github.com>
2026-06-13 20:25:44 +0530

    Update APPS_SCRIPT_URL in config.js

--- js/config.js
+++ js/config.js
@@ -25,7 +25,7 @@ const GJA_CONFIG = {
 
   // ── GOOGLE APPS SCRIPT WEB APP URL ──────────────────────
   // Step 3 ke baad milega ye URL
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz2ytkmCNam7ZeIG02pie789qd6fo1RGF3luWAjTKCSCFxHkAbgAjF37SSEv13PQkVV7Q/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwtNostUJqwJlo-DH89G7v1MG77kN1A-tch60AdiuUUfMHSW0WBYxKpN15TvBCqoEaS0A/exec",
 
   // ── BRANCH INFORMATION ──────────────────────────────────
   BRANCHES: {
